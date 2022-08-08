1810d42f03f582d6c8eedea7afa4691f4eab58e4
Artem Draft <Drafteed@users.noreply.github.com>
2022-08-09 01:01:36 +0300

    Library refactor (#4)

--- pybravia/__init__.py
+++ pybravia/__init__.py
@@ -1,10 +1,13 @@
+"""Python library for remote control of Sony Bravia TV."""
 # flake8: noqa
-from .braviatv import BraviaTV
+from .client import BraviaTV
 from .exceptions import (
     BraviaTVAuthError,
     BraviaTVConnectionError,
     BraviaTVConnectionTimeout,
     BraviaTVError,
+    BraviaTVNotFound,
+    BraviaTVNotSupported,
 )
 
-__version__ = "0.1.7"
+__version__ = "0.2.0"
